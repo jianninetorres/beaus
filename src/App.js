c0dcd5cbee25e3c7394c54d63bfde09a4ad89a26
jianninetorres <jianninetorres@gmail.com>
2018-05-03 22:57:05 -0400

    Minor UI changes, remove Beau's name from product list names

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ class App extends Component {
   }
 
   getProducts = async (e) => {
-    /* The json() method of the Body mixin takes a Response stream and reads it to completion. It returns a promise that resolves with the result of parsing the body text as JSON. */
     const api_call = await fetch(`https://lcboapi.com/products?access_key=${ACCESS_KEY}&q=beaus+all+natural+brewing&per_page=30`);
 
+    /* The json() method of the Body mixin takes a Response stream and reads it to completion. It returns a promise that resolves with the result of parsing the body text as JSON. */
     const data = await api_call.json();
 
     const products = data.result;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,8 +2,10 @@ import React from 'react';
 import '../css/product-list.css'
 
 const ProductList = (props) => {
-    const listItems = props.beerList.map((beer) => 
-        <li key={beer.id}>{beer.name}</li>
+    const listItems = props.beerList.map((beer) =>
+        beer.name.match(/beau's/i) 
+        ? <li key={beer.id}>{beer.name.replace(/beau's/i, '')}</li> 
+        : <li key={beer.id}>{beer.name}</li>,
       );
       return (
         <div className="product-list">
