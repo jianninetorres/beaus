499f5752f68bf5281ef92dd89680f37ec4f124cd
jianninetorres <jianninetorres@gmail.com>
2018-05-06 15:04:21 -0400

    Home page link, adjust image dimensions

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import '../css/logo.css';
 const Logo = () => {
     return (
         <div className="logo-box">
-            <h1>Beau's</h1>
+            <h1><a href="">Beau's</a></h1>
             <p>We are an award-winning, local, family-run, organic, and independent brewery.</p>
         </div>
     );
